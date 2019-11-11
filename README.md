74899c59e8db040557a2702bb0c8e22cf2aeea70
HiroyukiTonami <nyamu_z@yahoo.co.jp>
2019-11-11 09:36:02 +0900

    WIP

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 HNR
 ====
 
-csvファイルを読み込み、含まれる人名をランダムな異なる人名へ変換します。
+csvファイルを読み込み、含まれる人名を全て`'人名'`へ変換します。
 
 ## Usage
 
-- mecab本体をインストールします。[参考](https://qiita.com/yukinoi/items/990b6933d9f21ba0fb43)
+- mecab本体をインストールします。文字コード指定がある場合utf-8にしてください。[参考](https://qiita.com/yukinoi/items/990b6933d9f21ba0fb43)
+- インストールしたmecab.exeにPATHを通します。Windowsにデフォルトでインストールした場合`C:\Program Files\MeCab\bin`とかにあります。
 - `pip install -r requirements.txt`します。
 - human_name_removerと処理したいcsvファイルを同じフォルダに入れ、実行します。
+- 人名部分が全て置換されたresult.csvが出力されます。
--- human_name_remover.py
+++ human_name_remover.py
@@ -8,7 +8,7 @@ def search_target_paths(path):
     for pathname, dirnames, filenames in os.walk(path):
         for filename in filenames:
             if filename.endswith('.csv'):
-                target.append(os.path.join(pathname,filename))
+                target.append((filename[:-4], os.path.join(pathname,filename)))
 
     return target
 
@@ -20,25 +20,24 @@ def name_remove(text):
         wclass = node.feature.split(',')
         if wclass[0] != u'BOS/EOS':
             if wclass[2] == '人名':
-                word = '人名'
-            ret_text += word
+                text = text.replace(word, '人名')
         node = node.next
 
-    return ret_text
+    return text
 
 
 def main():
     target = search_target_paths(os.getcwd())
-    for fp in target:
+    for filename, fp in target:
         with open(fp, "r", newline="" ,encoding='utf-8') as csv_file:
-            reader = csv.reader(csv_file, delimiter=",", doublequote=True, quotechar='"', skipinitialspace=True)
+            reader = csv.reader(csv_file, delimiter=",")
             new_csv = []
             for row in reader:
                 new_row = ['']*len(row)
                 for i, column in enumerate(row):
                     new_row[i] = name_remove(column)
                 new_csv.append(new_row)
-        with open('result.csv', 'w', newline='', encoding='utf-8') as f:
+        with open(f'{filename}_result.csv', 'w', newline='', encoding='utf-8') as f:
                 writer = csv.writer(f)
                 writer.writerows(new_csv)
 
